0eec3d4e62dce129962994bb3dac3edef6709d09
sandi <sandisunandar99@gmail.com>
2020-10-06 16:17:47 +0700

    config mongodb connection

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { Router } = require('telegraf-router')
 const Telegraf = require('telegraf')
 const session = require('telegraf/session')
 const routes = require('./router')
+const mongoose = require('mongoose')
+const config = require('./config')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.use(session())
@@ -12,12 +14,16 @@ bot.use(async (ctx, next) => {
   require('./middleware/respose_time').responseTime(ctx, next)
 })
 
-new Router({
-  bot,
-  routes,
-  errorCb: ({ ctx }) => {
-    ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`)
-  }
-})
+mongoose.connect(config.database.uri, config.database.options, async (err, db) => {
+  if (err) console.log(err)
 
-bot.launch()
+  new Router({
+    bot,
+    routes,
+    errorCb: ({ ctx }) => {
+      ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`)
+    }
+  })
+
+  bot.launch()
+})
